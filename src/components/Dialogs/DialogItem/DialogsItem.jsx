33e4f175f0e460354cf37932c40c62f2155aa884
7scaiman <sanya.hanysh@gmail.com>
2022-12-22 22:58:43 +0000

    css update

--- src/components/Dialogs/DialogItem/DialogsItem.jsx
+++ src/components/Dialogs/DialogItem/DialogsItem.jsx
@@ -11,7 +11,9 @@ const DialogItem = (props) => {
        <NavLink to={"/Dialogs/" + props.name} className={SetActive}>
       <div className={css.dialogs} >
         <img src={props.logo} className={css.logo}/>
+        <div className={css.nickname} >
          {props.name}
+        </div>
         
     </div>
     </NavLink>
